ac202ce7e0b215b81af84bdf00fe1483fbf820e0
ClouD <ClouD>
2016-12-16 12:46:55 +0800

    initial commit

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
   var express = require("express");
     var app = express();
     var path = require('path');
+app.use(express.static(path.join(__dirname, 'public')))
 
-    app.get('/', function(req, res) {
-      res.send('<h1>Welcome to Elijah and Pier website!!!</h1>') ;
-    });
-
-    //app.get('/home', function(req, res) {
-    //	var absPath = path.join(__dirname,"profile/home.html");
-    //	console.log('absolute path is ' + absPath);
-    //	res.sendFile(absPath);
-    //});
 
      app.get('/home', function(req, res) {
     	var absPath = path.join(__dirname,"home.html");
@@ -18,6 +10,17 @@
     	res.sendFile(absPath);
     });
 
+      app.get('/member/deasis', function(req, res) {
+        var absPath = path.join(__dirname,"deasis.html");
+        console.log('absolute path is ' + absPath);
+        res.sendFile(absPath);
+    });
+
+       app.get('/member/martin', function(req, res) {
+        var absPath = path.join(__dirname,"martin.html");
+        console.log('absolute path is ' + absPath);
+        res.sendFile(absPath);
+    });
 
     var port = Number(process.env.PORT || 5000);
     app.listen(port, function() {
